b21e4ea574013312ccac6fa46e9f74148174d45d
gorle <249940@aa.com>
2021-08-14 02:21:55 +0530

    first commit

---
+++ src/test/java/com/gorley/listapp/ListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorley.listapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
