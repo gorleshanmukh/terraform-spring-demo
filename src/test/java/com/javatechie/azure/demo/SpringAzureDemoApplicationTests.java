f884cae5d26e7335029ae1aaffaa92e2ecbf7e22
gorle <249940@aa.com>
2021-08-14 02:47:49 +0530

    sad

--- src/test/java/com/javatechie/azure/demo/SpringAzureDemoApplicationTests.java
+++ src/test/java/com/javatechie/azure/demo/SpringAzureDemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.gorley.listapp;
+package com.javatechie.azure.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ListAppApplicationTests {
+class SpringAzureDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
